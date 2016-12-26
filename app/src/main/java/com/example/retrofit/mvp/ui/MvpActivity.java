13ae87329c2b4d94421355f23e3a41fa22ed56c2
wzgiceman <wzgiceman@gmail.com>
2016-12-26 14:36:38 +0800

    butterknife加入简化代码

--- app/src/main/java/com/example/retrofit/mvp/ui/MvpActivity.java
+++ app/src/main/java/com/example/retrofit/mvp/ui/MvpActivity.java
@@ -7,41 +7,43 @@ import android.widget.TextView;
 import com.example.retrofit.R;
 import com.example.retrofit.activity.BaseActivity;
 import com.example.retrofit.entity.api.SubjectPostApi;
-import com.example.retrofit.mvp.presenter.Plistener;
 import com.example.retrofit.mvp.presenter.P;
+import com.example.retrofit.mvp.presenter.Plistener;
 import com.wzgiceman.rxretrofitlibrary.retrofit_rx.exception.ApiException;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MvpActivity extends BaseActivity implements Vlistener {
-    TextView tvMsg,tvTest;
-    Plistener plistener;
+    @BindView(R.id.tv_test)
+    TextView tvTest;
+    @BindView(R.id.tv_msg)
+    TextView tvMsg;
+    private Plistener plistener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mvp);
-
-        tvTest=(TextView)findViewById(R.id.tv_test);
-
-
-        tvTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                plistener.doTest("1");
-            }
-        });
-
-
+        ButterKnife.bind(this);
         plistener = new P(this);
-        tvMsg = (TextView) findViewById(R.id.tv_msg);
-        tvMsg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SubjectPostApi postEntity = new SubjectPostApi();
-                postEntity.setAll(true);
-                plistener.startPost(MvpActivity.this, postEntity);
-            }
-        });
     }
+
+
+    @OnClick(value = R.id.tv_test)
+    void onTvTestClick(View view) {
+        plistener.doTest("1");
+    }
+
+
+    @OnClick(value = R.id.tv_msg)
+    void onTvMsgClick(View view) {
+        SubjectPostApi postEntity = new SubjectPostApi();
+        postEntity.setAll(true);
+        plistener.startPost(MvpActivity.this, postEntity);
+    }
+
 
     @Override
     public void showProg() {
@@ -65,7 +67,7 @@ public class MvpActivity extends BaseActivity implements Vlistener {
 
     @Override
     public void onTestNext(String msg) {
-        tvTest.setText("测试返回数据了："+msg);
+        tvTest.setText("测试返回数据了：" + msg);
     }
 
 }
